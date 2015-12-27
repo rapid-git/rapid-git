1a6e9869c9cee1981129dcb608bf4cf0113484d4
Alexander Groß <agross@therightstuff.de>
2015-12-27 16:00:41 +0100

    Move functions around to be grouped, adding some comments

--- rapid-git.sh
+++ rapid-git.sh
@@ -17,6 +17,37 @@ function rapid {
   local filter_staged='([MARC][ MD]|D[ M])'
   local filter_unmerged='(D[DU]|A[AU]|U[ADU])'
 
+  # Colors.
+  local c_end
+  local fg_black fg_red fg_green fg_yellow fg_blue fg_magenta fg_cyan fg_white
+  local fg_b_black fg_b_red fg_b_green fg_b_yellow fg_b_blue fg_b_magenta fg_b_cyan fg_b_white
+
+  function __rapid_init_colors {
+    # No colors when we're part of a pipeline or output is being redirected.
+    [[ -t 1 ]] || return
+
+    # Commented colors are not used. Speeds up things a bit on Windows where process creation is expensive.
+    c_end="$(git config --get-color "" "reset")"
+
+    # fg_black="$(git config --get-color "" "black")"
+    # fg_red="$(git config --get-color "" "red")"
+    # fg_green="$(git config --get-color "" "green")"
+    fg_yellow="$(git config --get-color "" "yellow")"
+    # fg_blue="$(git config --get-color "" "blue")"
+    # fg_magenta="$(git config --get-color "" "magenta")"
+    fg_cyan="$(git config --get-color "" "cyan")"
+    #fg_white="$(git config --get-color "" "white")"
+
+    # fg_b_black="$(git config --get-color "" "bold black")"
+    fg_b_red="$(git config --get-color "" "bold red")"
+    # fg_b_green="$(git config --get-color "" "bold green")"
+    fg_b_yellow="$(git config --get-color "" "bold yellow")"
+    # fg_b_blue="$(git config --get-color "" "bold blue")"
+    fg_b_magenta="$(git config --get-color "" "bold magenta")"
+    fg_b_cyan="$(git config --get-color "" "bold cyan")"
+    # fg_b_white="$(git config --get-color "" "bold white")"
+  }
+
   function __rapid_zsh {
     [[ -n "$ZSH_VERSION" ]]
   }
@@ -44,35 +75,6 @@ function rapid {
     done
   }
 
-  local c_end
-  local fg_black fg_red fg_green fg_yellow fg_blue fg_magenta fg_cyan fg_white
-  local fg_b_black fg_b_red fg_b_green fg_b_yellow fg_b_blue fg_b_magenta fg_b_cyan fg_b_white
-
-  function __rapid_init_colors {
-    [[ -t 1 ]] || return
-
-    # Commented colors are not used. Speeds up things a bit on Windows where process creation is expensive.
-    c_end="$(git config --get-color "" "reset")"
-
-    # fg_black="$(git config --get-color "" "black")"
-    # fg_red="$(git config --get-color "" "red")"
-    # fg_green="$(git config --get-color "" "green")"
-    fg_yellow="$(git config --get-color "" "yellow")"
-    # fg_blue="$(git config --get-color "" "blue")"
-    # fg_magenta="$(git config --get-color "" "magenta")"
-    fg_cyan="$(git config --get-color "" "cyan")"
-    #fg_white="$(git config --get-color "" "white")"
-
-    # fg_b_black="$(git config --get-color "" "bold black")"
-    fg_b_red="$(git config --get-color "" "bold red")"
-    # fg_b_green="$(git config --get-color "" "bold green")"
-    fg_b_yellow="$(git config --get-color "" "bold yellow")"
-    # fg_b_blue="$(git config --get-color "" "bold blue")"
-    fg_b_magenta="$(git config --get-color "" "bold magenta")"
-    fg_b_cyan="$(git config --get-color "" "bold cyan")"
-    # fg_b_white="$(git config --get-color "" "bold white")"
-  }
-
   function __rapid_command_not_found {
     local requested_command=$1
     local known_commands
@@ -87,6 +89,21 @@ function rapid {
 
     echo -e "Unknown command: ${1:-none}\n\nAvailable commands:\n$known_commands" 1>&2
     return 1
+  }
+
+  function __rapid_git_status {
+    # In bash we cannot store NULL characters in a variable. Go the extra mile and replace NULLs with \n.
+    # http://stackoverflow.com/q/6570531
+    # The pipefail option sets the exit code of the pipeline to the last program to exit non-zero or 0 if all succeed.
+    # http://unix.stackexchange.com/a/73180/72946
+    git_status="$(set -o pipefail; git status --porcelain -z | sed 's/\x0/\n/g')"
+  }
+
+  function __rapid_filter_git_status {
+    local git_status=$1
+    local filter="/^$2/!d"
+
+    printf "%s" "$(sed -$sedE "$filter" <<< "$git_status")"
   }
 
   function __rapid_query {
@@ -209,6 +226,7 @@ function rapid {
     done
   }
 
+  # Commands for the index.
   function __rapid__track {
     __rapid_git_status
     [[ $? -eq 0 ]] || return $?
@@ -309,7 +327,156 @@ function rapid {
     fi
   }
 
-  function __rapid__checkout {
+  function __rapid_status_of_type {
+    local header=$1
+    local git_status=$2
+    local filter=$3
+    local color=$4
+
+    local lines="$(__rapid_filter_git_status "$git_status" "$filter")"
+
+    if [[ -z "$lines" ]]; then
+      return
+    fi
+
+    # The other parameters are optional status replacements in the form of 'pattern' 'replacement'.
+    local prefixes
+    shift 4
+    while [[ $# -gt 0 ]]
+    do
+      prefixes+="s/\t$1\t/\t$2\t/;"
+      shift 2
+    done
+
+    local index_color=$fg_b_yellow
+
+    # No colors when we're part of a pipeline or output is being redirected.
+    local colorize
+    if [[ -t 1 ]]; then
+      colorize="s/^(.*)\t(.*)\t(.*)/$index_color\1$c_end\t$color\2$c_end\t$color\3$c_end/"
+    fi
+
+    local order_fields='s/^(.*)\t(.*)\t(.*)/  \2 \1 \3/'
+
+    local formatted="$(
+      sed -$sedE '
+        # Put index between lines: <status> <file> -> <index>\n<status> <file>
+        {=}
+        ' <<< "$lines" | \
+      sed --silent -$sedE -e '
+        # <index>\n<status> <file> -> <index>\t<status> <file>
+        {N;s/\n/\t/}
+      ' -e :a -e "
+        # Right-pad indexes shorter than three characters to three characters.
+        {s/^[ 0-9]{1,2}\t.*/ &/;ta}
+      " -e "
+        # <index>\t<status> <file> -> (<index>)\t<status>\t<file>
+        {s/^(( *)([1-9][0-9]*))\t(..) (.*)/\2(\3)\t\4\t\5/}
+
+        # Replace status with text, colorize fields, reorder fields.
+        {$prefixes;$colorize;$order_fields;p}"
+      )"
+
+    printf "%s:\n\n%s\n\n" "$header" "$formatted"
+  }
+
+  function __rapid__status {
+    __rapid_git_status
+    [[ $? -eq 0 ]] || return $?
+
+    __rapid_status_of_type 'Index - staged files' \
+      "$git_status" \
+      "$filter_staged" \
+      "$(git config --get-color color.status.added "bold green")" \
+      'M[MD ]'    'modified:        ' \
+      'A[MD ]'    'new file:        ' \
+      'D[M ]'     'deleted:         ' \
+      'R[MD ]'    'renamed:         ' \
+      'C[MD ]'    'copied:          '
+
+    __rapid_status_of_type 'Work tree - unstaged files' \
+      "$git_status" \
+      "$filter_unstaged" \
+      "$(git config --get-color color.status.changed "bold green")" \
+      '[MARC ]?M' 'modified:        ' \
+      '[MARC ]?D' 'deleted:         '
+
+    __rapid_status_of_type 'Untracked files' \
+      "$git_status" \
+      "$filter_untracked" \
+      "$(git config --get-color color.status.untracked "bold blue")" \
+      '\?\?'      'untracked file:  '
+
+    __rapid_status_of_type 'Unmerged files' \
+      "$git_status" \
+      "$filter_unmerged" \
+      "$fg_b_magenta" \
+      'UU'        'both modified:   ' \
+      'AA'        'both added:      ' \
+      'UA'        'added by them:   ' \
+      'AU'        'added by us:     ' \
+      'DD'        'both deleted:    ' \
+      'UD'        'deleted by them: ' \
+      'DU'        'deleted by us:   '
+  }
+
+  # Commands for branches.
+  function __rapid__branch {
+    local branches
+
+    if [[ "$1" == '-d' ]]; then
+
+      if [[ "$2" =~ ^[1-9][0-9]*$ ]]; then
+        branch="$(git branch | sed '/detached from/ d;' | sed -n "$2 !d;s/^..//;p")"
+
+        if [[ -z "$branch" ]]; then
+          echo -e "\t${fg_b_red}?$c_end Nothing on index $2."
+        else
+          git branch -d "$branch"
+          return $?
+        fi
+      else
+        echo -e "\t${fg_b_red}x$c_end Invalid input: $2."
+      fi
+
+    elif [[ "$1" == '-D' ]]; then
+
+      if [[ "$2" =~ ^[1-9][0-9]*$ ]]; then
+        branch="$(git branch | sed '/detached from/ d;' | sed -n "$2 !d;s/^..//;p")"
+
+        if [[ -z "$branch" ]]; then
+          echo -e "\t${fg_b_red}?$c_end Nothing on index $2."
+        else
+          git branch -D "$branch"
+          return $?
+        fi
+
+      else
+        echo -e "\t${fg_b_red}x$c_end Invalid input: $2."
+      fi
+
+    else
+      if [[ "$1" == '-a' ]]; then
+        branches="$(git branch -a)"
+      elif [[ "$1" == '-r' ]]; then
+        branches="$(git branch -r)"
+      else
+        branches="$(git branch)"
+      fi
+
+      [[ $? -eq 0 ]] || return $?
+
+      local detached="$(sed -n$sedE "/detached from/ !d;s/^\*/$fg_b_cyan>$c_end/;s/.$/&\\\\r\\\\n/;p" <<< "$branches")"
+      branches="$(sed '/detached from/ d' <<< "$branches" | sed = | sed '{N;s/\n/ /;}' | sed -e 's/^\([1-9][0-9]*\)  *\(.*\)/\2 \(\1\)/' | sed -n$sedE "s/^/  /;s/^  \*/$fg_b_cyan>$c_end/;s/\([1-9][0-9]*\)$/$fg_b_yellow&$c_end/;p" )"
+      printf "${detached}${branches}\r\n"
+
+      return 0
+    fi
+
+    return 1
+  }
+
+    function __rapid__checkout {
     local branches
     local line
 
@@ -384,166 +551,6 @@ function rapid {
       else
         echo -e "\t${fg_b_red}x$c_end Invalid input: $1."
       fi
-    fi
-
-    return 1
-  }
-
-  function __rapid_git_status {
-    # In bash we cannot store NULL characters in a variable. Go the extra mile and replace NULLs with \n.
-    # http://stackoverflow.com/q/6570531
-    # The pipefail option sets the exit code of the pipeline to the last program to exit non-zero or 0 if all succeed.
-    # http://unix.stackexchange.com/a/73180/72946
-    git_status="$(set -o pipefail; git status --porcelain -z | sed 's/\x0/\n/g')"
-  }
-
-  function __rapid_filter_git_status {
-    local git_status=$1
-    local filter="/^$2/!d"
-
-    printf "%s" "$(sed -$sedE "$filter" <<< "$git_status")"
-  }
-
-  function __rapid_status_of_type {
-    local header=$1
-    local git_status=$2
-    local filter=$3
-    local color=$4
-
-    local lines="$(__rapid_filter_git_status "$git_status" "$filter")"
-
-    if [[ -z "$lines" ]]; then
-      return
-    fi
-
-    # The other parameters are optional prefix replacements in the form of 'pattern' 'replacement'.
-    local prefixes
-    shift 4
-    while [[ $# -gt 0 ]]
-    do
-      prefixes+="s/\t$1\t/\t$2\t/;"
-      shift 2
-    done
-
-    local index_color=$fg_b_yellow
-    local colorize
-    if [[ -t 1 ]]; then
-      colorize="s/^(.*)\t(.*)\t(.*)/$index_color\1$c_end\t$color\2$c_end\t$color\3$c_end/"
-    fi
-    local order_fields='s/^(.*)\t(.*)\t(.*)/  \2 \1 \3/'
-
-    local formatted="$(
-      sed -$sedE '
-        # Put index between lines: <line> -> <index>\n<line>
-        {=}
-        ' <<< "$lines" | \
-      sed --silent -$sedE -e '
-        # <index>\n<status> <file> -> <index>\t<status> <file>
-        {N;s/\n/\t/}
-      ' -e :a -e "
-        # Right-pad indexes shorter than three characters to three characters.
-        {s/^[ 0-9]{1,2}\t.*/ &/;ta}
-      " -e "
-        # <index>\t<status> <file> -> (<index>)\t<status>\t<file>
-        {s/^(( *)([1-9][0-9]*))\t(..) (.*)/\2(\3)\t\4\t\5/}
-
-        # Replace status with text, colorize fields, reorder fields.
-        {$prefixes;$colorize;$order_fields;p}"
-      )"
-
-    printf "%s:\n\n%s\n\n" "$header" "$formatted"
-  }
-
-  function __rapid__status {
-    __rapid_git_status
-    [[ $? -eq 0 ]] || return $?
-
-    __rapid_status_of_type 'Index - staged files' \
-      "$git_status" \
-      "$filter_staged" \
-      "$(git config --get-color color.status.added "bold green")" \
-      'M[MD ]'    'modified:        ' \
-      'A[MD ]'    'new file:        ' \
-      'D[M ]'     'deleted:         ' \
-      'R[MD ]'    'renamed:         ' \
-      'C[MD ]'    'copied:          '
-
-    __rapid_status_of_type 'Work tree - unstaged files' \
-      "$git_status" \
-      "$filter_unstaged" \
-      "$(git config --get-color color.status.changed "bold green")" \
-      '[MARC ]?M' 'modified:        ' \
-      '[MARC ]?D' 'deleted:         '
-
-    __rapid_status_of_type 'Untracked files' \
-      "$git_status" \
-      "$filter_untracked" \
-      "$(git config --get-color color.status.untracked "bold blue")" \
-      '\?\?'      'untracked file:  '
-
-    __rapid_status_of_type 'Unmerged files' \
-      "$git_status" \
-      "$filter_unmerged" \
-      "$fg_b_magenta" \
-      'UU'        'both modified:   ' \
-      'AA'        'both added:      ' \
-      'UA'        'added by them:   ' \
-      'AU'        'added by us:     ' \
-      'DD'        'both deleted:    ' \
-      'UD'        'deleted by them: ' \
-      'DU'        'deleted by us:   '
-  }
-
-  function __rapid__branch {
-    local branches
-
-    if [[ "$1" == '-d' ]]; then
-
-      if [[ "$2" =~ ^[1-9][0-9]*$ ]]; then
-        branch="$(git branch | sed '/detached from/ d;' | sed -n "$2 !d;s/^..//;p")"
-
-        if [[ -z "$branch" ]]; then
-          echo -e "\t${fg_b_red}?$c_end Nothing on index $2."
-        else
-          git branch -d "$branch"
-          return $?
-        fi
-      else
-        echo -e "\t${fg_b_red}x$c_end Invalid input: $2."
-      fi
-
-    elif [[ "$1" == '-D' ]]; then
-
-      if [[ "$2" =~ ^[1-9][0-9]*$ ]]; then
-        branch="$(git branch | sed '/detached from/ d;' | sed -n "$2 !d;s/^..//;p")"
-
-        if [[ -z "$branch" ]]; then
-          echo -e "\t${fg_b_red}?$c_end Nothing on index $2."
-        else
-          git branch -D "$branch"
-          return $?
-        fi
-
-      else
-        echo -e "\t${fg_b_red}x$c_end Invalid input: $2."
-      fi
-
-    else
-      if [[ "$1" == '-a' ]]; then
-        branches="$(git branch -a)"
-      elif [[ "$1" == '-r' ]]; then
-        branches="$(git branch -r)"
-      else
-        branches="$(git branch)"
-      fi
-
-      [[ $? -eq 0 ]] || return $?
-
-      local detached="$(sed -n$sedE "/detached from/ !d;s/^\*/$fg_b_cyan>$c_end/;s/.$/&\\\\r\\\\n/;p" <<< "$branches")"
-      branches="$(sed '/detached from/ d' <<< "$branches" | sed = | sed '{N;s/\n/ /;}' | sed -e 's/^\([1-9][0-9]*\)  *\(.*\)/\2 \(\1\)/' | sed -n$sedE "s/^/  /;s/^  \*/$fg_b_cyan>$c_end/;s/\([1-9][0-9]*\)$/$fg_b_yellow&$c_end/;p" )"
-      printf "${detached}${branches}\r\n"
-
-      return 0
     fi
 
     return 1
